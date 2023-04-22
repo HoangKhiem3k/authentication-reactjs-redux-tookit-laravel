2774dacf2f267fe2a9e2de3069a3c0a53ef0d59a
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2023-04-22 16:12:12 +0700

    done

--- frontend/src/redux/types/authType.js
+++
@@ -1,7 +0,0 @@
-// login constants
-export const LOGIN_START = "LOGIN_START";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_ERROR = "LOGIN_ERROR";
-
-// logout constants
-export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
